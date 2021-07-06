61e552c6fc5810ec2f16c3b7fb88734c1727161f
marcus bianchi <marcusbianchi92@hotmail.com>
2021-07-06 18:49:22 -0300

    Adicionar arquivos de projeto.

---
+++ 07.Manual.Exercise.01.DATETIME/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Entities;
+namespace _07.Manual.Exercise._01.DATETIME {
+    class Program {
+        static void Main(string[] args) {
+            DateTime d1 = new DateTime(2000, 8, 15);
+            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58);
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58, 275);   
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+
+            DateTime d4 = DateTime.Now;
+            DateTime d5 = DateTime.UtcNow;
+            DateTime d6 = DateTime.Today;
+            Console.WriteLine(d4);
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
+
+            DateTime d7 = DateTime.Parse("2000-08-15");
+            DateTime d8 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime d9 = DateTime.Parse("15/18/2000");
+            DateTime d10 = DateTime.Parse("15/08/2000 13:05:58");
+            Console.WriteLine(d7);
+            Console.WriteLine(d8);
+            Console.WriteLine(d9);
+            Console.WriteLine(d10);
+
+            DateTime d11 = DateTime.Parse("2000-08-15", "yyyy-MM-dd");
+
+        }
+    }
+}
